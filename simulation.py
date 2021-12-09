53434c9142a725438d96aec855b7a1e37750a28e
Nickibra <nicolas.ibrahim@epfl.ch>
2021-12-09 18:10:09 +0100

    18:09 commit

--- simulation.py
+++ simulation.py
@@ -17,7 +17,7 @@ longueur = 40
 Noyau.initialisation(largeur, hauteur, longueur)
 
 # Graphe 3D
-'''
+
 from mpl_toolkits.mplot3d import Axes3D
 print("sim_A")
 fig = plt.figure()
@@ -63,7 +63,7 @@ def animation_frame(i):
 	Y_air = Noyau.coord_Yair()
 	Z_air = Noyau.coord_Zair()
 	#couleur_air = Noyau.getCouleur_air() si on fait ?
-
+	
 	#ax.scatter(X_sol, Y_sol, Z_sol, c = 'brown', marker='s', s = 10, alpha=0.3)
 	#ax.scatter(X_air, Y_air, Z_air, c = 'skyblue', marker='s', s = 10, alpha=0.01) #voir comment on fait la couleur de l'air avec l'héritage
 	#ax.scatter(X_eau, Y_eau, Z_eau, c = 'blue', marker='s', s = 10, alpha=0.2)
@@ -72,7 +72,9 @@ def animation_frame(i):
 	ax.scatter(X_air, Y_air, Z_air, c = 'skyblue', marker='s', s = 10, alpha=0.002) #voir comment on fait la couleur de l'air avec l'héritage -> alpha = 0,001 l'air disparait
 	ax.scatter(X_eau, Y_eau, Z_eau, c = couleur_eau, cmap = "Blues", marker='s', s = 10, alpha=0.2, vmin = 0, vmax = 100)
 	Noyau.ecoulement(i)
-line, ax.plot(X_sol+X_eau+X_air, Y_sol+Y_eau+Y_air, Z_sol+Z_eau+Z_air)
+	
+	
+
 	# ou cmap = plt.cm.get_cmap("blues", 5) le 5 est la séparation de l'échelle des couleurs
 	# donc pour si on veut des séparation marquée ou tout continue et donc peut être moins flagrant l'avancée mais plus réelle l'avancée
   
@@ -83,22 +85,15 @@ print("sim_C")
 #remplacer les couleurs par cmap ou jsais pas quoi qu'on peut transferer dans le csv.
   # cmap = "blues"
 
-animation = FuncAnimation(fig, func = animation_frame, frames = np.arange(0, 10, 0.01), interval = 1000, blit =True)
+animation = FuncAnimation(fig, func = animation_frame, frames = np.arange(0, 10, 0.01), interval = 1000, blit =False)
 plt.show()
-'''
 
+'''
 from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
-ax.set_title('Belle rivière !')
-ax.set_xlabel('Longueur')
-ax.set_xlim(0,40)
-ax.set_ylabel('Largeur')
-ax.set_ylim(-5,25)
-ax.set_zlabel('Hauteur')
-ax.set_zlim(0,40)
+scat=ax.scatter(X_sol, Y_sol, Z_sol, c = couleur_sol, cmap = "copper", marker='s', s = 10, alpha=0.3, vmin = 0, vmax = 100)
 
 def init(): 
 	X_eau = Noyau.coord_Xeau()
@@ -143,3 +138,12 @@ def animation_frame(i):
 
 animation = FuncAnimation(fig, func = animation_frame, init_func = init, frames = np.arange(0, 10, 0.01), interval = 1000, blit =False)
 plt.show()
+
+ax.set_title('Belle rivière !')
+ax.set_xlabel('Longueur')
+ax.set_xlim(0,40)
+ax.set_ylabel('Largeur')
+ax.set_ylim(-5,25)
+ax.set_zlabel('Hauteur')
+ax.set_zlim(0,40)
+'''

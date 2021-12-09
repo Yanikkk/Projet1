5cd02b22b4b8a0722512e1c858a8d9d143293019
Yanikkk <yanik.haas@epfl.ch>
2021-12-09 23:02:23 +0100

    simulation ok ? commit

--- simulation.py
+++ simulation.py
@@ -18,6 +18,7 @@ Noyau.initialisation(largeur, hauteur, longueur)
 
 # Graphe 3D
 
+'''
 from mpl_toolkits.mplot3d import Axes3D
 print("sim_A")
 fig = plt.figure()
@@ -72,72 +73,14 @@ def animation_frame(i):
 	ax.scatter(X_air, Y_air, Z_air, c = 'skyblue', marker='s', s = 10, alpha=0.002) #voir comment on fait la couleur de l'air avec l'héritage -> alpha = 0,001 l'air disparait
 	ax.scatter(X_eau, Y_eau, Z_eau, c = couleur_eau, cmap = "Blues", marker='s', s = 10, alpha=0.2, vmin = 0, vmax = 100)
 	Noyau.ecoulement(i)
-	
-	
-
-	# ou cmap = plt.cm.get_cmap("blues", 5) le 5 est la séparation de l'échelle des couleurs
-	# donc pour si on veut des séparation marquée ou tout continue et donc peut être moins flagrant l'avancée mais plus réelle l'avancée
-  
-#	print("sim_B")
-#	return ax,
-	
-print("sim_C")
-#remplacer les couleurs par cmap ou jsais pas quoi qu'on peut transferer dans le csv.
-  # cmap = "blues"
 
 animation = FuncAnimation(fig, func = animation_frame, frames = np.arange(0, 10, 0.01), interval = 1000, blit =False)
 plt.show()
-
 '''
-from mpl_toolkits.mplot3d import Axes3D
 
+from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-scat=ax.scatter(X_sol, Y_sol, Z_sol, c = couleur_sol, cmap = "copper", marker='s', s = 10, alpha=0.3, vmin = 0, vmax = 100)
-
-def init(): 
-	X_eau = Noyau.coord_Xeau()
-	Y_eau = Noyau.coord_Yeau()
-	Z_eau = Noyau.coord_Zeau()
-	couleur_eau = Noyau.getCouleur_eau()
-
-	X_sol = Noyau.coord_Xsol()
-	Y_sol = Noyau.coord_Ysol()
-	Z_sol = Noyau.coord_Zsol()
-	couleur_sol = Noyau.getCouleur_sol()
-
-	X_air = Noyau.coord_Xair()
-	Y_air = Noyau.coord_Yair()
-	Z_air = Noyau.coord_Zair()
-	
-	ax.scatter(X_sol, Y_sol, Z_sol, c = couleur_sol, cmap = "copper", marker='s', s = 10, alpha=0.3, vmin = 0, vmax = 100)
-	ax.scatter(X_air, Y_air, Z_air, c = 'skyblue', marker='s', s = 10, alpha=0.002) 
-	ax.scatter(X_eau, Y_eau, Z_eau, c = couleur_eau, cmap = "Blues", marker='s', s = 10, alpha=0.2, vmin = 0, vmax = 100)
-
-def animation_frame(i):
-
-	Noyau.ecoulement(i)
-	
-	X_eau = Noyau.coord_Xeau()
-	Y_eau = Noyau.coord_Yeau()
-	Z_eau = Noyau.coord_Zeau()
-	couleur_eau = Noyau.getCouleur_eau()
-
-	X_sol = Noyau.coord_Xsol()
-	Y_sol = Noyau.coord_Ysol()
-	Z_sol = Noyau.coord_Zsol()
-	couleur_sol = Noyau.getCouleur_sol()
-
-	X_air = Noyau.coord_Xair()
-	Y_air = Noyau.coord_Yair()
-	Z_air = Noyau.coord_Zair()
-
-	ax.scatter(X_sol, Y_sol, Z_sol, c = couleur_sol, cmap = "copper", marker='s', s = 10, alpha=0.3, vmin = 0, vmax = 100)
-	ax.scatter(X_air, Y_air, Z_air, c = 'skyblue', marker='s', s = 10, alpha=0.002) 
-	ax.scatter(X_eau, Y_eau, Z_eau, c = couleur_eau, cmap = "Blues", marker='s', s = 10, alpha=0.2, vmin = 0, vmax = 100)
-
-animation = FuncAnimation(fig, func = animation_frame, init_func = init, frames = np.arange(0, 10, 0.01), interval = 1000, blit =False)
-plt.show()
 
 ax.set_title('Belle rivière !')
 ax.set_xlabel('Longueur')
@@ -146,4 +89,90 @@ ax.set_ylabel('Largeur')
 ax.set_ylim(-5,25)
 ax.set_zlabel('Hauteur')
 ax.set_zlim(0,40)
-'''
+
+X_sol = []
+Y_sol = []
+Z_sol = []
+X_air = []
+Y_air = []
+Z_air = []
+X_eau = []
+Y_eau = []
+Z_eau = []
+#besoin de tout ça ? 
+
+#retourne les coordonnées x des cases du tableau contenant l'eau à l'instant i
+X_eau = Noyau.coord_Xeau()
+Y_eau = Noyau.coord_Yeau()
+Z_eau = Noyau.coord_Zeau()
+couleur_eau = Noyau.getCouleur_eau()
+
+X_sol = Noyau.coord_Xsol()
+Y_sol = Noyau.coord_Ysol()
+Z_sol = Noyau.coord_Zsol()
+couleur_sol = Noyau.getCouleur_sol()
+
+X_air = Noyau.coord_Xair()
+Y_air = Noyau.coord_Yair()
+Z_air = Noyau.coord_Zair()
+#couleur_air = Noyau.getCouleur_air() si on fait ?
+
+scatter_sol = ax.scatter(X_sol, Y_sol, Z_sol, c = couleur_sol, cmap = "copper", marker='s', s = 10, alpha=0.3, vmin = 0, vmax = 100)
+scatter_air = ax.scatter(X_air, Y_air, Z_air, c = 'skyblue', marker='s', s = 10, alpha=0.002) #voir comment on fait la couleur de l'air avec l'héritage -> alpha = 0,001 l'air disparait
+scatter_eau = ax.scatter(X_eau, Y_eau, Z_eau, c = couleur_eau, cmap = "Blues", marker='s', s = 10, alpha=0.2, vmin = 0, vmax = 100)
+
+def setup_plot():
+	X_eau = Noyau.coord_Xeau()
+	Y_eau = Noyau.coord_Yeau()
+	Z_eau = Noyau.coord_Zeau()
+	couleur_eau = Noyau.getCouleur_eau()
+
+	X_sol = Noyau.coord_Xsol()
+	Y_sol = Noyau.coord_Ysol()
+	Z_sol = Noyau.coord_Zsol()
+	couleur_sol = Noyau.getCouleur_sol()
+
+	X_air = Noyau.coord_Xair()
+	Y_air = Noyau.coord_Yair()
+	Z_air = Noyau.coord_Zair()
+	#couleur_air = Noyau.getCouleur_air() si on fait ?
+
+	scatter_sol = ax.scatter(X_sol, Y_sol, Z_sol, c = couleur_sol, cmap = "copper", marker='s', s = 10, alpha=0.3, vmin = 0, vmax = 100)
+	scatter_air = ax.scatter(X_air, Y_air, Z_air, c = 'skyblue', marker='s', s = 10, alpha=0.002) #voir comment on fait la couleur de l'air avec l'héritage -> alpha = 0,001 l'air disparait
+	scatter_eau = ax.scatter(X_eau, Y_eau, Z_eau, c = couleur_eau, cmap = "Blues", marker='s', s = 10, alpha=0.2, vmin = 0, vmax = 100)
+	
+	scatters = [scatter_sol, scatter_eau, scatter_air]
+	return scatters
+
+def animation_frame(i):
+	#fait avancer les cases EAU
+	
+	Noyau.ecoulement(i)
+	
+	X_eau = Noyau.coord_Xeau()
+	Y_eau = Noyau.coord_Yeau()
+	Z_eau = Noyau.coord_Zeau()
+	couleur_eau = Noyau.getCouleur_eau()
+	#pas besoin je pense de couleur
+
+	X_sol = Noyau.coord_Xsol()
+	Y_sol = Noyau.coord_Ysol()
+	Z_sol = Noyau.coord_Zsol()
+	couleur_sol = Noyau.getCouleur_sol()
+	#pas besoin je pense de couleur 
+
+	X_air = Noyau.coord_Xair()
+	Y_air = Noyau.coord_Yair()
+	Z_air = Noyau.coord_Zair()
+	#couleur_air = Noyau.getCouleur_air() si on fait ?
+	
+	scatter_eau._offsets3d = (X_eau, Y_eau, Z_eau)
+	scatter_sol._offsets3d = (X_sol, Y_sol, Z_sol)
+	scatter_air._offsets3d = (X_air, Y_air, Z_air)
+	
+	scatters = [scatter_sol, scatter_eau, scatter_air]
+	return scatters
+
+animation = FuncAnimation(fig, func=animation_frame, init_func=setup_plot, frames=np.arange(0, 10, 0.01), interval=1000, blit=True)
+plt.show()
+

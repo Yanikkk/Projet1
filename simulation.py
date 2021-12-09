45a9a6e44d1604421e3b117c1d7ef59c4d0ad782
Yanikkk <yanik.haas@epfl.ch>
2021-12-09 16:22:44 +0100

    simulation commit

--- simulation.py
+++ simulation.py
@@ -5,9 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-largeur = 2
-hauteur = 5
-longueur = 10
+largeur = 20
+hauteur = 30
+longueur = 40
 	#avec taille !!! 20,30,40 (ne marche pas au niveau de Case::setX(x) (il ne trouve pas l attribut privé
 	#40,60,200
 	
@@ -17,7 +17,7 @@ longueur = 10
 Noyau.initialisation(largeur, hauteur, longueur)
 
 # Graphe 3D
-
+'''
 from mpl_toolkits.mplot3d import Axes3D
 print("sim_A")
 fig = plt.figure()
@@ -84,4 +84,62 @@ print("sim_C")
   # cmap = "blues"
 
 animation = FuncAnimation(fig, func = animation_frame, frames = np.arange(0, 10, 0.01), interval = 1000, blit =True)
+plt.show()
+'''
+
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.set_title('Belle rivière !')
+ax.set_xlabel('Longueur')
+ax.set_xlim(0,40)
+ax.set_ylabel('Largeur')
+ax.set_ylim(-5,25)
+ax.set_zlabel('Hauteur')
+ax.set_zlim(0,40)
+
+def init(): 
+	X_eau = Noyau.coord_Xeau()
+	Y_eau = Noyau.coord_Yeau()
+	Z_eau = Noyau.coord_Zeau()
+	couleur_eau = Noyau.getCouleur_eau()
+
+	X_sol = Noyau.coord_Xsol()
+	Y_sol = Noyau.coord_Ysol()
+	Z_sol = Noyau.coord_Zsol()
+	couleur_sol = Noyau.getCouleur_sol()
+
+	X_air = Noyau.coord_Xair()
+	Y_air = Noyau.coord_Yair()
+	Z_air = Noyau.coord_Zair()
+	
+	ax.scatter(X_sol, Y_sol, Z_sol, c = couleur_sol, cmap = "copper", marker='s', s = 10, alpha=0.3, vmin = 0, vmax = 100)
+	ax.scatter(X_air, Y_air, Z_air, c = 'skyblue', marker='s', s = 10, alpha=0.002) 
+	ax.scatter(X_eau, Y_eau, Z_eau, c = couleur_eau, cmap = "Blues", marker='s', s = 10, alpha=0.2, vmin = 0, vmax = 100)
+
+def animation_frame(i):
+
+	Noyau.ecoulement(i)
+	
+	X_eau = Noyau.coord_Xeau()
+	Y_eau = Noyau.coord_Yeau()
+	Z_eau = Noyau.coord_Zeau()
+	couleur_eau = Noyau.getCouleur_eau()
+
+	X_sol = Noyau.coord_Xsol()
+	Y_sol = Noyau.coord_Ysol()
+	Z_sol = Noyau.coord_Zsol()
+	couleur_sol = Noyau.getCouleur_sol()
+
+	X_air = Noyau.coord_Xair()
+	Y_air = Noyau.coord_Yair()
+	Z_air = Noyau.coord_Zair()
+
+	ax.scatter(X_sol, Y_sol, Z_sol, c = couleur_sol, cmap = "copper", marker='s', s = 10, alpha=0.3, vmin = 0, vmax = 100)
+	ax.scatter(X_air, Y_air, Z_air, c = 'skyblue', marker='s', s = 10, alpha=0.002) 
+	ax.scatter(X_eau, Y_eau, Z_eau, c = couleur_eau, cmap = "Blues", marker='s', s = 10, alpha=0.2, vmin = 0, vmax = 100)
+
+animation = FuncAnimation(fig, func = animation_frame, init_func = init, frames = np.arange(0, 10, 0.01), interval = 1000, blit =False)
 plt.show()

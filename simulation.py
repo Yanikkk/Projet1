bb64e78f17c1c146cfb7573f58df3588c40b3f83
Yanikkk <yanik.haas@epfl.ch>
2021-12-14 15:25:11 +0100

    rien changé

--- simulation.py
+++ simulation.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import keyboard
 
-largeur = 20
-hauteur = 30
-longueur = 40
+largeur = 2
+hauteur = 5
+longueur = 12
 
 	#avec taille !!! 20,30,40 (ne marche pas au niveau de Case::setX(x) (il ne trouve pas l attribut privé
 	#40,60,200
